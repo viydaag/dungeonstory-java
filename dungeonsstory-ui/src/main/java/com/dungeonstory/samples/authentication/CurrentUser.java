febbc90d2d42e3b8e6a7be46fd66454887f91e1a
Jean-Christophe Fortier <jean-christophe.fortier@desjardins.com>
2016-06-27 08:28:33 -0400

    modif CurrentUser

--- dungeonsstory-ui/src/main/java/com/dungeonstory/samples/authentication/CurrentUser.java
+++ dungeonsstory-ui/src/main/java/com/dungeonstory/samples/authentication/CurrentUser.java
@@ -1,67 +1,24 @@
 package com.dungeonstory.samples.authentication;
 
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinService;
+import com.dungeonstory.samples.backend.data.User;
+import com.vaadin.server.VaadinSession;
 
 /**
- * Class for retrieving and setting the name of the current user of the current
- * session (without using JAAS). All methods of this class require that a
- * {@link VaadinRequest} is bound to the current thread.
- * 
- * 
- * @see com.vaadin.server.VaadinService#getCurrentRequest()
+ * Convenience wrapper for storing and retreiving a user from the VaadinSession
  */
-public final class CurrentUser {
+public class CurrentUser {
 
-    /**
-     * The attribute key used to store the username in the session.
-     */
-    public static final String CURRENT_USER_SESSION_ATTRIBUTE_KEY = CurrentUser.class
-            .getCanonicalName();
+    private static final String KEY = "currentser";
 
-    private CurrentUser() {
+    public static void set(User user) {
+        VaadinSession.getCurrent().setAttribute(KEY, user);
     }
 
-    /**
-     * Returns the name of the current user stored in the current session, or an
-     * empty string if no user name is stored.
-     * 
-     * @throws IllegalStateException
-     *             if the current session cannot be accessed.
-     */
-    public static String get() {
-        String currentUser = (String) getCurrentRequest().getWrappedSession()
-                .getAttribute(CURRENT_USER_SESSION_ATTRIBUTE_KEY);
-        if (currentUser == null) {
-            return "";
-        } else {
-            return currentUser;
-        }
+    public static User get() {
+        return (User) VaadinSession.getCurrent().getAttribute(KEY);
     }
 
-    /**
-     * Sets the name of the current user and stores it in the current session.
-     * Using a {@code null} username will remove the username from the session.
-     * 
-     * @throws IllegalStateException
-     *             if the current session cannot be accessed.
-     */
-    public static void set(String currentUser) {
-        if (currentUser == null) {
-            getCurrentRequest().getWrappedSession().removeAttribute(
-                    CURRENT_USER_SESSION_ATTRIBUTE_KEY);
-        } else {
-            getCurrentRequest().getWrappedSession().setAttribute(
-                    CURRENT_USER_SESSION_ATTRIBUTE_KEY, currentUser);
-        }
-    }
-
-    private static VaadinRequest getCurrentRequest() {
-        VaadinRequest request = VaadinService.getCurrentRequest();
-        if (request == null) {
-            throw new IllegalStateException(
-                    "No request bound to current thread");
-        }
-        return request;
+    public static boolean isLoggedIn() {
+        return get() != null;
     }
 }
