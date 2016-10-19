ef7a0d5fc6c517255cc52eaf169b50b6a61932e6
Jean-Christophe Fortier <jean-christophe.fortier@desjardins.com>
2016-10-19 08:31:06 -0400

    remove unused methods

--- dungeonsstory-backend/src/main/java/com/dungeonstory/backend/repository/impl/ClassRepository.java
+++ dungeonsstory-backend/src/main/java/com/dungeonstory/backend/repository/impl/ClassRepository.java
@@ -1,9 +1,5 @@
 package com.dungeonstory.backend.repository.impl;
 
-import javax.persistence.Query;
-
-import com.dungeonstory.backend.Configuration;
-import com.dungeonstory.backend.data.ClassLevelFeature;
 import com.dungeonstory.backend.data.DSClass;
 import com.dungeonstory.backend.repository.AbstractRepository;
 
@@ -16,21 +12,4 @@ public class ClassRepository extends AbstractRepository<DSClass, Long> {
         return DSClass.class;
     }
     
-    public void deleteClassLevelBonusFeat(ClassLevelFeature bonusFeat) {
-        if (bonusFeat == null) {
-            return;
-        }
-        entityManager.getTransaction().begin();
-        Query query = entityManager.createNamedQuery(DSClass.deleteClassLevelBonusFeat);
-        query.setParameter("classe", bonusFeat.getClasse());
-        query.setParameter("level", bonusFeat.getLevel());
-        query.setParameter("feat", bonusFeat.getFeat());
-        int result = query.executeUpdate();
-        if (Configuration.getInstance().isDebug()) {
-            System.out.println("Nombre de class level bonus feat deleted = " + result);
-        }
-        entityManager.getTransaction().commit();
-    }
-    
-
 }
