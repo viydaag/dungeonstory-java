3764dd8d04560c66f5c7b8b92dd902d2e6be578b
JC Fortier <fortier.jc@gmail.com>
2016-12-04 00:15:57 -0500

    add new images and set character portrait

---
+++ dungeonstory-ui/src/main/java/com/dungeonstory/DSConstant.java
@@ -0,0 +1,23 @@
+package com.dungeonstory;
+
+import com.vaadin.server.VaadinService;
+
+public final class DSConstant {
+    
+    private DSConstant() {
+        
+    }
+    
+    public static String getBaseDir() {
+        return VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+    }
+    
+    public static String getImageDir() {
+        return getBaseDir() + "/WEB-INF/images";
+    }
+    
+    public static String getSoundDir() {
+        return getBaseDir() + "/WEB-INF/sound";
+    }
+
+}
--- dungeonstory-ui/src/main/java/com/dungeonstory/view/character/InformationForm.java
+++ dungeonstory-ui/src/main/java/com/dungeonstory/view/character/InformationForm.java
@@ -1,6 +1,8 @@
 package com.dungeonstory.view.character;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.vaadin.peter.imagestrip.ImageStrip;
 import org.vaadin.viritin.fields.IntegerField;
@@ -8,6 +10,7 @@ import org.vaadin.viritin.fields.MTextField;
 import org.vaadin.viritin.fields.TypedSelect;
 import org.vaadin.viritin.form.AbstractForm.SavedHandler;
 
+import com.dungeonstory.DSConstant;
 import com.dungeonstory.backend.data.Alignment;
 import com.dungeonstory.backend.data.Character;
 import com.dungeonstory.backend.data.Character.Gender;
@@ -19,7 +22,6 @@ import com.dungeonstory.form.DSAbstractForm;
 import com.dungeonstory.util.ImageFilter;
 import com.vaadin.data.Validator;
 import com.vaadin.server.FileResource;
-import com.vaadin.server.VaadinService;
 import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.FormLayout;
@@ -36,24 +38,30 @@ public class InformationForm extends DSAbstractForm<Character> implements SavedH
     private TextField              height;
     private TypedSelect<Alignment> alignment;
     private TypedSelect<Region>    region;
-    private ImageStrip             image;
+    private ImageStrip             imageStrip;
+
+    private Map<ImageStrip.Image, String> imageMap;
+    private String                        image;
 
     private AlignmentDataService alignmentService = AlignmentService.getInstance();
-    private RegionService    regionService    = RegionService.getInstance();
+    private RegionService        regionService    = RegionService.getInstance();
+    
+    private FormLayout layout;
 
     public InformationForm() {
         super();
         setSavedHandler(this);
+        imageMap = new HashMap<ImageStrip.Image, String>();
     }
 
     @Override
     public void onSave(Character entity) {
-
+        entity.setImage(image);
     }
 
     @Override
     protected Component createContent() {
-        FormLayout layout = new FormLayout();
+        layout = new FormLayout();
         layout.setMargin(new MarginInfo(true, true));
 
         name = new MTextField("Nom").withWidth("250px");
@@ -83,38 +91,61 @@ public class InformationForm extends DSAbstractForm<Character> implements SavedH
             }
         });
 
-        image = new ImageStrip(org.vaadin.peter.imagestrip.ImageStrip.Alignment.HORIZONTAL);
-        // Use animation
-        image.setAnimated(true);
+        gender.addMValueChangeListener(event -> initImageStrip(event.getValue()));
 
-        // Make strip to behave like select
-        image.setSelectable(true);
+        initImageStrip(null);
 
-        // Set size of the box surrounding the images
-        image.setImageBoxWidth(140);
-        image.setImageBoxHeight(140);
-
-        // Set maximum size of the images
-        image.setImageMaxWidth(125);
-        image.setImageMaxHeight(125);
-
-        // Limit how many images are visible at most simultaneously
-        image.setMaxAllowed(6);
-
-        // Add few images to the strip using different methods
-        String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
-        File imageDir = new File(basepath + "/WEB-INF/images/");
-
-        if (imageDir.isDirectory()) { // make sure it's a directory
-            for (final File imageFile : imageDir.listFiles(new ImageFilter())) {
-                FileResource resource = new FileResource(imageFile);
-                image.addImage(resource);
-            }
-        }
-
-        layout.addComponents(name, gender, age, weight, height, alignment, region, image);
+        layout.addComponents(name, gender, age, weight, height, alignment, region, imageStrip);
 
         return layout;
+    }
+
+    private void initImageStrip(Gender gender) {
+        if (imageStrip != null) {
+            layout.removeComponent(imageStrip);
+        }
+        
+        imageStrip = new ImageStrip(org.vaadin.peter.imagestrip.ImageStrip.Alignment.HORIZONTAL);
+        imageStrip.setHeight(200, Unit.PIXELS);
+        imageStrip.setCaption("Portrait");
+
+        // Use animation
+        imageStrip.setAnimated(true);
+
+        // Make strip to behave like select
+        imageStrip.setSelectable(true);
+
+        // Add ValueChangeListener to listen for image selection
+        imageStrip.addValueChangeListener(event -> {
+            ImageStrip.Image selectedImage = (ImageStrip.Image) event.getProperty().getValue();
+            image = imageMap.get(selectedImage);
+        });
+
+        // Set size of the box surrounding the images
+        imageStrip.setImageBoxWidth(120);
+        imageStrip.setImageBoxHeight(180);
+
+        // Set maximum size of the images
+        imageStrip.setImageMaxWidth(110);
+        imageStrip.setImageMaxHeight(170);
+
+        // Limit how many images are visible at most simultaneously
+        imageStrip.setMaxAllowed(7);
+
+        if (gender != null && gender.getImageDir() != null) {
+            File imageDir = new File(DSConstant.getImageDir() + "/" + gender.getImageDir());
+
+            if (imageDir.isDirectory()) { // make sure it's a directory
+                for (final File imageFile : imageDir.listFiles(new ImageFilter())) {
+                    FileResource resource = new FileResource(imageFile);
+                    ImageStrip.Image image = imageStrip.addImage(resource);
+                    imageMap.put(image, "/" + gender.getImageDir() + "/" + imageFile.getName());
+                }
+            }
+        }
+        
+        layout.addComponent(imageStrip);
+        
     }
 
     @Override
